6f4c41b5b60fdcef8f10675c46ee9e47e749320f
Manish Kumar <kumarmanish.gkv@gmail.com>
2021-11-16 13:29:11 +0530

    Updates

--- analytic-edge-ws-react_weather_app-ef29593a9ec8/weather-app/src/reducers/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { weatherInfo } from "./weaherReducer";
-
-// combine reducers
-const reducers = combineReducers({
-  WeatherInfo: weatherInfo,
-});
-
-export default reducers;
